7023e8b857f6ae2c3dc3eb7bb1d8030817a4e344
Lakee Rawat <73211332+lakeerawat@users.noreply.github.com>
2021-12-29 23:55:52 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReverse
+{
+ class Program
+    {
+        static string ReverseString(string input)
+        {
+            if (string.IsNullOrEmpty(input)) { return input; }
+           StringBuilder result = new StringBuilder();
+            string[] arr = input.Split(" ");
+            for (int i = 0; i < arr.Length ; i++)
+            {
+                result.Append(ReverseString(arr[i]));
+                if(i != arr.Length - 1)
+                {
+                    result.Append(" ");
+                }
+            }
+            return result.ToString().ToLower();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseString("Welocome to the world"));
+        }
+    }
+}
